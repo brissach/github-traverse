b15c765ff0a6e280db56cd5952ec34f146ef0fee
Clouke <76826179+Clouke@users.noreply.github.com>
2023-08-12 20:26:28 +0200

    `Marker` for applying the access token to the reader

---
+++ src/main/java/gg/clouke/gtraverse/TokenMarker.java
@@ -0,0 +1,10 @@
+package gg.clouke.gtraverse;
+
+/**
+ * @author Clouke
+ * @since 12.08.2023 10:13
+ * © github-traverse - All Rights Reserved
+ */
+public interface TokenMarker {
+  void setToken(String token);
+}
